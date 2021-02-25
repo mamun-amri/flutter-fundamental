4bfe301d201642544e3106f097e5791f3d3b677f
tafrij <mamunamri97@gmail.com>
2021-02-25 13:39:13 +0700

    tambah navigation bottom

--- latihan_tabbar/lib/main_page.dart
+++ latihan_tabbar/lib/main_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+
 // import 'package:carousel_slider/carousel_slider.dart';
+
+var _selectedIndex = 0;
 
 class MainPage extends StatelessWidget {
   @override
@@ -8,6 +11,16 @@ class MainPage extends StatelessWidget {
     var grey = Color(0xffc4c4c4);
     var black = Color(0xff383d43);
     var white = Color(0xffffffff);
+
+    const _TextStyle = TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+
+    const List<Widget> _bottomNavigationOptions = [
+      Text("Home", style: _TextStyle),
+      Text("Location", style: _TextStyle),
+      Text("Box", style: _TextStyle),
+      Text("History", style: _TextStyle),
+      Text("Account", style: _TextStyle),
+    ];
 
     return MaterialApp(
         home: DefaultTabController(
@@ -39,200 +52,239 @@ class MainPage extends StatelessWidget {
             ],
           ),
         ),
-        body: TabBarView(
+        body: Stack(
           children: [
-            // tab featured
-            Container(
-              margin: EdgeInsets.fromLTRB(30, 20, 30, 10),
-              child: Column(
-                children: [
-                  // slider
-                  Container(
-                    height: MediaQuery.of(context).size.height * 0.2,
-                    width: MediaQuery.of(context).size.width * 1,
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(4),
-                        color: black,
-                        image: DecorationImage(
-                            image: AssetImage("assets/slide1.jpg"),
-                            fit: BoxFit.cover)),
-                    margin: EdgeInsets.fromLTRB(0, 10, 0, 10),
-                  ),
-                  // menu
-                  Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        buildRow(black, "Chair", "assets/1.jpg"),
-                        buildRow(black, "Shopa", "assets/2.jpg"),
-                        buildRow(black, "Pasions", "assets/3.jpg"),
-                      ]),
-                  Container(
-                    margin: EdgeInsets.zero,
-                    child: Flexible(
-                      child: Row(children: [
-                        Flexible(
-                            flex: 2,
-                            child:
-                                buildRow(black, "Plastation", "assets/4.jpg")),
-                        Flexible(
-                            flex: 3,
-                            child: buildRow(black, "Earphone", "assets/5.jpg")),
-                      ]),
-                    ),
-                  ),
-                  Container(
-                    margin: EdgeInsets.fromLTRB(0, 20, 0, 10),
-                    alignment: Alignment.topLeft,
-                    child: Text(
-                      "Best Seller",
-                      style: TextStyle(
-                          fontSize: 15,
-                          color: black,
-                          fontWeight: FontWeight.bold),
-                    ),
-                  ),
-                  //best seller
-                  Column(
+            TabBarView(
+              children: [
+                // tab featured
+                Container(
+                  margin: EdgeInsets.fromLTRB(30, 20, 30, 10),
+                  child: Column(
                     children: [
-                      Material(
-                        color: Colors.transparent,
-                        elevation: 2,
-                        borderRadius: BorderRadius.circular(4),
-                        child: Container(
-                          decoration: BoxDecoration(
+                      // slider
+                      Container(
+                        height: MediaQuery.of(context).size.height * 0.2,
+                        width: MediaQuery.of(context).size.width * 1,
+                        decoration: BoxDecoration(
                             borderRadius: BorderRadius.circular(4),
-                            color: white,
-                          ),
-                          child: Row(
-                            children: [
-                              Container(
-                                height: 70,
-                                width: 70,
-                                margin: EdgeInsets.fromLTRB(10, 10, 10, 10),
-                                decoration: BoxDecoration(
-                                  color: grey,
-                                  image: DecorationImage(
-                                    image: AssetImage("assets/6.jpg"),
-                                    fit: BoxFit.cover,
-                                  ),
-                                ),
+                            color: black,
+                            image: DecorationImage(
+                                image: AssetImage("assets/slide1.jpg"),
+                                fit: BoxFit.cover)),
+                        margin: EdgeInsets.fromLTRB(0, 10, 0, 10),
+                      ),
+                      // menu
+                      Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            buildRow(black, "Chair", "assets/1.jpg"),
+                            buildRow(black, "Shopa", "assets/2.jpg"),
+                            buildRow(black, "Pasions", "assets/3.jpg"),
+                          ]),
+                      Container(
+                        margin: EdgeInsets.zero,
+                        child: Flexible(
+                          child: Row(children: [
+                            Flexible(
+                                flex: 2,
+                                child: buildRow(
+                                    black, "Plastation", "assets/4.jpg")),
+                            Flexible(
+                                flex: 3,
+                                child: buildRow(
+                                    black, "Earphone", "assets/5.jpg")),
+                          ]),
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.fromLTRB(0, 20, 0, 10),
+                        alignment: Alignment.topLeft,
+                        child: Text(
+                          "Best Seller",
+                          style: TextStyle(
+                              fontSize: 15,
+                              color: black,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      //best seller
+                      Column(
+                        children: [
+                          Material(
+                            color: Colors.transparent,
+                            elevation: 2,
+                            borderRadius: BorderRadius.circular(4),
+                            child: Container(
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(4),
+                                color: white,
                               ),
-                              Column(
-                                mainAxisAlignment: MainAxisAlignment.start,
-                                crossAxisAlignment: CrossAxisAlignment.start,
+                              child: Row(
                                 children: [
-                                  // nama barang dan warna
-                                  Row(
-                                    children: [
-                                      Text(
-                                        "Elevator",
-                                        style: TextStyle(
-                                            color: black,
-                                            fontWeight: FontWeight.bold,
-                                            fontSize: 20),
-                                      )
-                                    ],
-                                  ),
-                                  // detail barang
-                                  Padding(
-                                    padding:
-                                        const EdgeInsets.fromLTRB(0, 5, 0, 5),
-                                    child: Text(
-                                      "lorem ipsum any thing that",
-                                      style: TextStyle(
-                                          color: black,
-                                          fontWeight: FontWeight.bold,
-                                          fontSize: 10),
+                                  Container(
+                                    height: 70,
+                                    width: 70,
+                                    margin: EdgeInsets.fromLTRB(10, 10, 10, 10),
+                                    decoration: BoxDecoration(
+                                      color: grey,
+                                      image: DecorationImage(
+                                        image: AssetImage("assets/6.jpg"),
+                                        fit: BoxFit.cover,
+                                      ),
                                     ),
                                   ),
-                                  // harga
-                                  Row(
+                                  Column(
+                                    mainAxisAlignment: MainAxisAlignment.start,
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
                                     children: [
-                                      Text(
-                                        "Rp. 100.000",
-                                        style: TextStyle(
-                                            color: black,
-                                            fontWeight: FontWeight.bold,
-                                            fontSize: 10,
-                                            fontStyle: FontStyle.italic,
-                                            decoration:
-                                                TextDecoration.lineThrough,
-                                            decorationColor: Color(0xffd32f3f)),
+                                      // nama barang dan warna
+                                      Row(
+                                        children: [
+                                          Text(
+                                            "Elevator",
+                                            style: TextStyle(
+                                                color: black,
+                                                fontWeight: FontWeight.bold,
+                                                fontSize: 20),
+                                          )
+                                        ],
                                       ),
-                                      Container(
-                                        margin:
-                                            EdgeInsets.fromLTRB(20, 0, 0, 0),
+                                      // detail barang
+                                      Padding(
+                                        padding: const EdgeInsets.fromLTRB(
+                                            0, 5, 0, 5),
                                         child: Text(
-                                          "Rp. 89.000",
+                                          "lorem ipsum any thing that",
                                           style: TextStyle(
                                               color: black,
                                               fontWeight: FontWeight.bold,
-                                              fontSize: 20),
+                                              fontSize: 10),
                                         ),
+                                      ),
+                                      // harga
+                                      Row(
+                                        children: [
+                                          Text(
+                                            "Rp. 100.000",
+                                            style: TextStyle(
+                                                color: black,
+                                                fontWeight: FontWeight.bold,
+                                                fontSize: 10,
+                                                fontStyle: FontStyle.italic,
+                                                decoration:
+                                                    TextDecoration.lineThrough,
+                                                decorationColor:
+                                                    Color(0xffd32f3f)),
+                                          ),
+                                          Container(
+                                            margin: EdgeInsets.fromLTRB(
+                                                20, 0, 0, 0),
+                                            child: Text(
+                                              "Rp. 89.000",
+                                              style: TextStyle(
+                                                  color: black,
+                                                  fontWeight: FontWeight.bold,
+                                                  fontSize: 20),
+                                            ),
+                                          )
+                                        ],
                                       )
                                     ],
                                   )
                                 ],
-                              )
-                            ],
-                          ),
-                        ),
+                              ),
+                            ),
+                          )
+                        ],
                       )
                     ],
-                  )
-                ],
-              ),
+                  ),
+                ),
+                // tab room
+                buildRow(black, "Badcooper", "assets/3.jpg"),
+                // tab style
+                buildRow(black, "Badcooper", "assets/3.jpg"),
+              ],
             ),
-            // tab room
-            buildRow(black, "Badcooper", "assets/3.jpg"),
-            // tab style
-            buildRow(black, "Badcooper", "assets/3.jpg"),
+            _bottomNavigationOptions.elementAt(_selectedIndex),
           ],
+        ),
+        // navigasi bawah
+        bottomNavigationBar: BottomNavigationBar(
+          items: [
+            BottomNavigationBarItem(
+                icon: Icon(Icons.home_outlined, color: black), label: "Home"),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.location_on_outlined, color: black),
+                label: "Location"),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.add_box_outlined, color: black), label: "Box"),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.history_outlined, color: black),
+                label: "History"),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.person_outline, color: black),
+                label: "Account"),
+          ],
+          currentIndex: _selectedIndex,
+          selectedItemColor: Colors.amber[800],
+          onTap: _selectedTap,
         ),
       ),
     ));
   }
+}
 
-  Row buildRow(Color black, String text, String img) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        Column(
-          children: [
-            Container(
-              margin: EdgeInsets.fromLTRB(0, 10, 0, 10),
-              height: 75,
-              width: 75,
-              child: Material(
-                color: Colors.transparent,
-                elevation: 5,
-                child: Image(
-                  image: AssetImage(img),
-                  fit: BoxFit.cover,
-                ),
+void _selectedTap(int index) {
+  setState(() {
+    _selectedIndex = index;
+  });
+}
+
+void setState(VoidCallback fn) {
+  return fn();
+}
+
+Row buildRow(Color black, String text, String img) {
+  return Row(
+    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    children: [
+      Column(
+        children: [
+          Container(
+            margin: EdgeInsets.fromLTRB(0, 10, 0, 10),
+            height: 75,
+            width: 75,
+            child: Material(
+              color: Colors.transparent,
+              elevation: 5,
+              child: Image(
+                image: AssetImage(img),
+                fit: BoxFit.cover,
               ),
             ),
-            Container(
-                child: Text(
-              text,
-              style: TextStyle(fontWeight: FontWeight.w400, color: black),
-            ))
-          ],
-        ),
-      ],
-    );
-  }
-
-  Tab buildTab(Color black, String text) {
-    return Tab(
-      child: Text(
-        text,
-        style: TextStyle(
-            fontFamily: "Roboto",
-            color: black,
-            fontSize: 20,
-            fontWeight: FontWeight.w500),
+          ),
+          Container(
+              child: Text(
+            text,
+            style: TextStyle(fontWeight: FontWeight.w400, color: black),
+          ))
+        ],
       ),
-    );
-  }
+    ],
+  );
+}
+
+Tab buildTab(Color black, String text) {
+  return Tab(
+    child: Text(
+      text,
+      style: TextStyle(
+          fontFamily: "Roboto",
+          color: black,
+          fontSize: 20,
+          fontWeight: FontWeight.w500),
+    ),
+  );
 }
